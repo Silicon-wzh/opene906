bde2089da35eeb80160863dc65343233b118586d
wuzhenghui <ahu_hui@163.com>
2022-06-21 00:40:29 +0800

    support shell

--- E906_RTL_FACTORY/setup/setup.sh
+++ E906_RTL_FACTORY/setup/setup.sh
@@ -1,7 +1,7 @@
-#!/bin/csh
+#!/bin/bash -e
 
 set pattern = "\/setup"`echo '$'`
-setenv CODE_BASE_PATH `pwd | perl -pe "s/$pattern//"`
+export CODE_BASE_PATH=`pwd | perl -pe "s/$pattern//"`
 echo "Root of code base has been specified as:\n    $CODE_BASE_PATH"
 
 
---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./E906_RTL_FACTORY/
+
+source setup/setup.sh
+
+cd ../smart_run/
+
+source setup/example_setup.sh
